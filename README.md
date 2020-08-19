80d3775518d29680e0b2dd24ccd79a213620c9b8
Justyna Pietrzak <tynk713@gmail.com>
2020-08-19 22:43:29 +0200

    Setup contenerization with docker

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cars_api
+docker-compose run web python manage.py migrate
+docker-compose up
--- cars/settings.py
+++ cars/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'cars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'PASSWORD': 'somepassword',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
